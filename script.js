24054cfc9eaf4b5a5ded1fcb4c0b8a57f9bb5c73
Glitch (workspaces) <none>
2022-10-09 15:45:58 -0700

    ADD
    Added vaiables for different digits.
    Added console.log messages

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ console.log('greetings from script.js');
 
 function convertToRoman(num) {
     let answer = "";
-    let fourDig = 0;
-    let threeDig = 0;
-    let twoDig = 0;
-    let oneDig = 0;
+    let fourDig, threeDig, twoDig, oneDig = 0;
+    //let threeDig = 0;
+    //let twoDig = 0;
+    //let oneDig = 0;
 
 
-
+    console.log("fourDig = " + fourDig);
+    console.log("threeDig = " + threeDig);
+    console.log("twoDig = " + twoDig);
+    console.log("oneDig = " + oneDig);
     return answer;
    }
    
