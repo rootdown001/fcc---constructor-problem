4ec7853c825ded6412fa88847ef91078bd08d81e
Glitch (workspaces) <none>
2022-10-08 18:32:48 -0700

    make a person

--- script.js
+++ script.js
@@ -4,41 +4,17 @@ console.log('greetings from script.js');
 //function onClickEvent() {
 // *****************
 
-function truthCheck(collection, pre) {
+const Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  this.getFullName = function() {
+    return "";
+  };
+  return firstAndLast;
+};
 
-
-  let answer;
-// let arr = [{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}];
-
-
-for (let i in collection) {
-  //console.log(collection[i])
-  //console.log(collection[i][pre])
-
-  if (collection[i][pre]) {    
-    console.log("yes");
-    answer = true;
-  } else {
-    answer = false;
-    return answer
-  }
-
-
-} 
-
-return answer;
-
-}
-
-
-
-
-console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
-
-console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role"));
-
-console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"));
-
+const bob = new Person('Bob Ross');
+bob.getFullName();
 
 
 // *****************
